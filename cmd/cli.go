c1c66dbc71dbcb6bceed8ce16bcc9b4394bd159b
mawngo <sitdownrightnow.dev@gmail.com>
2024-06-27 17:40:19 +0700

    feat(wip): scanning input directories

--- cmd/cli.go
+++ cmd/cli.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/mawngo/piconic/internal/scan"
 	"github.com/phsym/console-slog"
 	"github.com/spf13/cobra"
 	"log/slog"
@@ -28,6 +29,13 @@ type CLI struct {
 // NewCLI create new CLI instance and setup application config.
 func NewCLI() *CLI {
 	level := Init()
+
+	f := flags{
+		Size:    200,
+		Output:  ".",
+		Padding: 10,
+	}
+
 	command := cobra.Command{
 		Use:   "piconic [files...]",
 		Short: "Generate icon from images",
@@ -42,16 +50,45 @@ func NewCLI() *CLI {
 			}
 			return nil
 		},
-		Run: func(_ *cobra.Command, _ []string) {
-			slog.Info("Hello World!")
+		Run: func(cmd *cobra.Command, args []string) {
+			now := time.Now()
+			if _, err := os.Stat(f.Output); err != nil {
+				err := os.Mkdir(f.Output, os.ModePerm)
+				if err != nil {
+					slog.Info("Error creating output directory", slog.Any("dir", f.Output))
+					return
+				}
+			}
+
+			for _, arg := range args {
+				for img := range scan.Img(arg) {
+					process(f, img)
+				}
+			}
+
+			slog.Info("Processing completed", slog.Duration("took", time.Since(now)))
 		},
 	}
+
+	command.Flags().UintVarP(&f.Size, "size", "s", f.Size, "Size of the output image")
+	command.Flags().StringVarP(&f.Output, "out", "o", f.Output, "Output directory name")
+	command.Flags().IntVarP(&f.Padding, "padding", "p", f.Padding, "Padding of the image (by % of the size)")
 	command.PersistentFlags().Bool("debug", false, "Enable debug mode")
 	return &CLI{&command}
+}
+
+type flags struct {
+	Size    uint
+	Output  string
+	Padding int
 }
 
 func (cli *CLI) Execute() {
 	if err := cli.command.Execute(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 	}
+}
+
+func process(flags flags, img scan.DecodedImage) {
+
 }
---
+++ internal/scan/scan.go
@@ -0,0 +1,91 @@
+package scan
+
+import (
+	"fmt"
+	"image"
+	"log/slog"
+	"os"
+	"path/filepath"
+)
+
+func Img(dir string) <-chan DecodedImage {
+	ch := make(chan DecodedImage, 1)
+	info, err := os.Stat(dir)
+	if err != nil {
+		slog.Error("Err scanning file(s)", slog.String("path", dir), slog.Any("err", err))
+		close(ch)
+		return ch
+	}
+
+	go func() {
+		defer close(ch)
+		if !info.IsDir() {
+			img, err := decode(dir)
+			if err != nil {
+				slog.Error("Err decoding image", slog.String("path", dir), slog.Any("err", err))
+				return
+			}
+			ch <- img
+			return
+		}
+
+		files, err := os.ReadDir(".")
+		if err != nil {
+			slog.Error("Err scanning dir", slog.String("path", dir), slog.Any("err", err))
+			return
+		}
+
+		for _, file := range files {
+			if file.IsDir() {
+				continue
+			}
+			path := filepath.Join(dir, file.Name())
+			img, err := decode(path)
+			if err != nil {
+				slog.Error("Not a image", slog.String("path", path), slog.Any("err", err))
+				continue
+			}
+			ch <- img
+		}
+	}()
+
+	return ch
+}
+
+func decode(path string) (DecodedImage, error) {
+	img := DecodedImage{
+		Path: path,
+	}
+	f, err := os.Open(path)
+	if err != nil {
+		return img, err
+	}
+	defer f.Close()
+
+	config, _, err := image.DecodeConfig(f)
+	if err != nil {
+		return img, err
+	}
+	img.Config = config
+
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		panic(err)
+	}
+	slog.Debug("Decoding image", slog.String("path", path), slog.String("dimension", fmt.Sprintf("%dx%d", img.Config.Width, img.Config.Height)))
+	imageData, imageType, err := image.Decode(f)
+	if err != nil {
+		return img, err
+	}
+	img.Type = imageType
+	img.Image = imageData
+
+	return img, nil
+}
+
+type DecodedImage struct {
+	image.Image
+	image.Config
+	Type string
+	Path string
+}
